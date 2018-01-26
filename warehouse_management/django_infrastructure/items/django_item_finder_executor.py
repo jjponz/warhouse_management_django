32efadc1d484ce66c32babb53c524a79aa589bc2
Juanjo Ponz <jjponzva@gmail.com>
2018-01-26 01:11:27 +0100

    Add last test to django item finder executor

--- warehouse_management/django_infrastructure/items/django_item_finder_executor.py
+++ warehouse_management/django_infrastructure/items/django_item_finder_executor.py
@@ -3,13 +3,9 @@ from warehouse_management.django_infrastructure import DjangoItemRepository
 from warehouse_management.business_logic.actions import ItemFinder
 
 class DjangoItemFinderExecutor:
-    def __init__(self, form):
-        self.__form = form
+    def __init__(self):
         self.__adapter = DjangoItemAdapter()
         self.__action = ItemFinder(DjangoItemRepository())
 
-    def do(self):
-        property = self.__form.data['property']
-        value = self.__form.data['value']
-
+    def do(self, property, value):
         return self.__action.do(property, value)
--- warehouse_management/tests/integrations/actions/items/test_django_item_finder_executor_should.py
+++ warehouse_management/tests/integrations/actions/items/test_django_item_finder_executor_should.py
@@ -7,18 +7,25 @@ from warehouse_management.django_infrastructure import DjangoItemFinderExecutor
 class TestDjangoItemFinderExecutorShould(TestCase):
     def test_find_items_by_name(self):
         self.__init_repository()
-        item_form = ItemFinderForm(data={'property':'name', 'value':'Juanjo'})
-        django_item_finder_executor = DjangoItemFinderExecutor (item_form)
+        item_finder_form = ItemFinderForm(data={'property':'name', 'value':'Juanjo'})
+        django_item_finder_executor = DjangoItemFinderExecutor ()
 
-        finded_items = django_item_finder_executor.do()
+        finded_items = django_item_finder_executor.do(item_finder_form.data['property'], item_finder_form.data['value'])
 
         self.assertEqual(1, len(finded_items))
 
+    def test_find_items_by_notes(self):
+        self.__init_repository()
+        item_finder_form = ItemFinderForm(data={'property':'notes', 'value':'some note'})
+        django_item_finder_executor = DjangoItemFinderExecutor ()
 
+        finded_items = django_item_finder_executor.do(item_finder_form.data['property'], item_finder_form.data['value'])
+
+        self.assertEqual(1, len(finded_items))
 
     def __init_repository(self):
         self.__item_repository = DjangoItemRepository()
-        item = ItemBuilder().with_name("Juanjo").with_generated_uid().build()
+        item = ItemBuilder().with_name("Juanjo").with_generated_uid().with_notes("Some note").build()
         self.__item_repository.save(item)
         items = ItemBuilder().build_random(3)
         self.__item_repository.add_range(items)
