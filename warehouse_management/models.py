4039bfdab11b764d3960bcb09f1f34282a853051
Juanjo Ponz <jjponzva@gmail.com>
2018-03-10 21:30:46 +0100

    django admin show items instead of item mapper

--- warehouse_management/models.py
+++ warehouse_management/models.py
@@ -4,3 +4,6 @@ class ItemMapper(models.Model):
     name = models.CharField(default="", max_length=255)
     notes = models.CharField(default="", max_length=1000)
     uid = models.CharField(max_length=100, primary_key=True)
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
